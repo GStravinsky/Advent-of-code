8b8c48715055b8691a5477905d839f115ce49199
gstravinskaite <g.stravinskaite@elsevier.com>
2020-12-08 15:57:00 +0100

    day 7

--- day7/day7.py
+++ day7/day7.py
@@ -2,59 +2,54 @@ data = open('data.txt')
 data = data.read()
 
 data = data.split('\n')
-print(len(data))
+
 
 def get_bag(sentance):
     words = sentance.split(' ')
     return words[2]
 
-number_of_bags = []
 
 has_gold = [x for x in data if "shiny gold" in x]
-number_of_bags.append(len(has_gold))
-
 first_comp = [x for x in data if x not in has_gold]
+print(has_gold)
 
 def get_colours(rule):
   words = rule.split(' ')
   colours = ' '.join(words[:2])
   return colours
 
-first_cols = [get_colours(x) for x in has_gold]
-
+first_cols = [get_colours(x) for x in has_gold if "shiny gold" not in x]
 # second round
 
-print(len(first_cols))
+print(first_cols)
 
-
-def number_of_colors(first_cols, data, first_comp):
+def number_of_colors(first_cols, first_comp):
   cols = first_cols
   comp = first_comp
-  number = []
-  n = 0
-  while n < 1:
-    n = 1
+  number = [len(has_gold)]
+  while len(cols)>0:
+    # list to hold all the unique lines that contains colours c
     combined_lines = []
     for c in cols:
-      lines_have_color_c = [ x for x in comp if str(c) in x]
-      lines_have_color_c = [x for x in lines_have_color_c if len(x)>0]
-      print(lines_have_color_c)
-      cols2 = [get_colours(x) for x in lines_have_color_c if len(x)>0]
-      cols2 = list(set(cols2))
-      combined_lines.append(cols2)
-      print(combined_lines)
-    combined_colors = list(set(combined_lines))
-    print(combined_colors)
-    cols = list(set(cols))
+        print(c)
+        lines_have_color_c = [ x for x in comp if str(c) in x]
+        print(lines_have_color_c)
+        lines_have_color_c = [x for x in lines_have_color_c if len(x)>0]
+        print(lines_have_color_c)
+        combined_lines.append(lines_have_color_c)
+    # flatten list and retain only unique lines
+    combined_lines = list(set([l for sublist in combined_lines for l in sublist]))      
+    print(combined_lines)
+    cols = [get_colours(x) for x in combined_lines]
     print(cols)
-    comp = [x for x in data if x not in combined_lines]
-    
-  return combined_colors
+    comp = [l for l in comp if l not in combined_lines]
+    print(comp)
+    number.append(len(cols))
+      
+  return number
+#no2 = number_of_colors(first_cols, first_comp)
 
-no2 = number_of_colors(first_cols, data, first_comp)
+#print(no2)
 
-print(no2)
-
-print(sum(no2)+len(has_gold))
 
 
