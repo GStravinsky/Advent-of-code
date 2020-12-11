3dd6162bf7178a777d76cddf62b7f2fa67f36eb4
gstravinskaite <g.stravinskaite@elsevier.com>
2020-12-11 17:41:03 +0100

    Empty trials

--- day8/day8.py
+++ day8/day8.py
@@ -48,7 +48,7 @@ def calc_acc_one_loop(data):
     no_duplicates = 0
     index = 0
     acc = 0
-    while no_duplicates == 0:
+    while no_duplicates <=1:
       command, number = command_parser(data[index])
       if command == "acc":
         acc, index = acc_do(acc, number, index)
@@ -61,28 +61,54 @@ def calc_acc_one_loop(data):
       
       indices.append(index)       
       no_duplicates = count_duplicates(indices)
-    print(acc)     
+      print(acc, no_duplicates, index)
     return acc, no_duplicates
 
 def jmp_to_nop(data, index):
   data[index] = "nop%s" %data[index][3:] 
   return data
 
- 
-print(len(data)) 
+### PART 2 ###
+
+def islastcommand642(data):
+    data.append("")
+    index = 0
+    acc = 0
+    no_duplicates = 0
+    indices = []
+    while index <= 642 and no_duplicates == 0:
+
+      command, number = command_parser(data[index])
+      if command == "acc":
+        acc, index = acc_do(acc, number, index)
+      elif command == "jmp":
+        index = jmp_do(number, index)
+    
+      elif command == "nop":
+        index = nop_do(index)
+      indices.append(index)
+      no_duplicates = count_duplicates(indices)
+    return acc, index
+    
+     
+def nop_to_jmp(data, index):
+    data[index] = "jmp%s" %data[index][3:]
+    return data
 def fix_loop(data):
   acc = 0
-  for i in range(len(data)):
-    print(acc)
+  for i in range(10):
+    print("index", i)
+    print("acc", acc)
     command, number = command_parser(data[i])
     print(command, number)
-    if command == "jmp":
-       data = jmp_to_nop(data,i)
-       acc, no_duplicates = calc_acc_one_loop(data)
-       if no_duplicates == 0:
-        break
-  return acc, i
+    if command == "nop":
+        data2 = nop_to_jmp(data,i)
+        print(data2[:10])
+        print("Adjusted string", data2[i])
+        acc, index = islastcommand642(data2)
+        if index == 642:
+         print("This one", index, acc)
+    
+  return acc
 
-data2 = jmp_to_nop(data,3)
-print(data2[3])
-print(calc_acc_one_loop(data2))
+print(fix_loop(data))
