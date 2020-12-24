02ccc315a76ff02dbf50fa365e451f7f51fb3a45
Gabrielė Stravinskaitė <stravinskaite.g@gmail.com>
2020-12-24 16:25:02 +0100

    whatever

--- day11/day11.py
+++ day11/day11.py
@@ -14,14 +14,13 @@ data2 = np.empty((90,92), dtype=object)
 for r in range(len(data)):
     data2[r] = [1 if i == "L" else "." for i in data[r]]
 
-print(data2)
 def summarise_neighbours(data, row, column):
     neighbours = []
     if row == 0 and column == 0:
         neighbours.append(data[row,column+1])
         neighbours.append(data[row+1,column+1])
         neighbours.append(data[row+1,column])
-    elif row == 0 and column > 0 and column < (np.shape(data2)[1]-1):
+    elif row == 0 and column != 0 and column != (np.shape(data)[1]-1):
         neighbours.append(data[row,column+1])
         neighbours.append(data[row+1,column+1])
         neighbours.append(data[row+1,column])
@@ -69,25 +68,35 @@ def summarise_neighbours(data, row, column):
     count_neighbours = Counter(neighbours)
     return count_neighbours[1]
 
-print(summarise_neighbours(data2,0,4))
 
 def one_iteration(data):
-    data_new = data
-    for row_n, row in enumerate(data_new):
+    data_new = np.copy(data)
+    for row_n, row in enumerate(data):
       for column_n, column in enumerate(row):
         # if the seat is empty and has no neighbours - it becomes occupied
-        if column == 0 and summarise_neighbours(data_new, row_n, column_n) == 0:
-            data_new[row_n,column_n] == 1
+        count = summarise_neighbours(data,row_n,column_n)
+        if column == 0 and count == 0:
+            data_new[row_n,column_n] = 1
         # if the seat is occupied and has more or exactly 4 neighbours - it becomes empty
-        elif column == 1 and summarise_neighbours(data_new, row_n, column_n) >= 4:
-            data_new[row_n,column_n] == 0
-    return data_new
-
-print(one_iteration(data2))
-
+        elif column == 1 and count >= 4:
+          data_new[row_n,column_n] = 0
+    count = np.count_nonzero(data_new == 1)
+    return data_new, count
 
 
+  
+def all_iterations(data):
+  # setting dummy values
+  count = 0
+  diff = 1
+  while diff  != 0:
+    data_new, count_new = one_iteration(data)
+    diff = count-count_new
+    count = count_new
+    print(count)
+    data = np.copy(data_new)
+  return(count)
+
+print(all_iterations(data2))
+ 
 ## if column == 'L" and the count==0 column == Occupies
-## if column =="Occupied" and the count>=4 column == L
-## if column == ., skip. 
-# make sure you do the changes on a NEW MAP 
